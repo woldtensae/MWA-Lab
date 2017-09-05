2e75c5aae073edb72f27cfcde79768a4b44d60fb
unknown <108735@CS.MUM.EDU>
2017-09-05 15:50:05 -0500

    exercies 4

--- Lab1/exercise4.js
+++ Lab1/exercise4.js
@@ -1,3 +1,11 @@
-String.prototype.filter = function(str){
-    var myStr = Array.from(this);
+
+String.prototype.filter = function(words){
+    var myStr = this;
+    for(let word in words){
+        myStr.replace(word, '*****');
+    }
+    return myStr; 
 }
+
+console.log('this house is nice'.filter(['nice', 'house']));
+
